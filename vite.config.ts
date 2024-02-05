c235b404a5fd0b2676592d1104e7000f9a548a02
Robert Zakirov <ra.20.z02@mail.ru>
2024-02-06 00:31:18 +0300

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/free-to-play-games/',
   server: {
-    port: 3001
-  }
-})
+    port: 3001,
+  },
+});
